77b06dbddb9833ffc949dd9b45a46c07e42274ae
Connor Davis <19422@kloofhigh.co.za>
2021-11-21 15:15:38 +0200

    still fixing auth

--- state/gun.js
+++ state/gun.js
@@ -1,7 +1,7 @@
 import Gun from 'gun/gun';
 import "gun/sea";
 
-let gun = new Gun({ peers: [], axe: false });
+let gun = new Gun({ peers: ['https://localhost:8765/gun'], axe: false, localStorage: true });
 
 let user = gun.user().recall({ sessionStorage: true });
 
