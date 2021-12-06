ec4bd3b61afc0f120fc61b9b630643b71f0e2dd6
Connor Davis <19422@kloofhigh.co.za>
2021-12-06 09:31:45 +0200

    Added chats logic

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import * as authentication from "./lib/authentication";
 import * as certificates from "./lib/certificates";
 import * as friends from "./lib/friends";
+import * as messaging from "./lib/messaging";
 import { gun, user } from "./state/gun";
 
-export { authentication, certificates, friends, gun, user };
+export { authentication, certificates, friends, messaging, gun, user };
 
