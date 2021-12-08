6d3b88da8af9f1c249bc50675ef0135d8cca9adf
Connor Davis <19422@kloofhigh.co.za>
2021-12-08 11:58:56 +0200

    Update for end-to-end encryption

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ import * as friends from "./lib/friends";
 import * as messaging from "./lib/messaging";
 import { gun, user } from "./state/gun";
 
+console.log.bind(console);
+
 export { authentication, certificates, friends, messaging, gun, user };
 
--- state/gun.js
+++ state/gun.js
@@ -1,8 +1,19 @@
-import Gun from 'gun/gun';
+import Gun from "gun/gun";
 import "gun/sea";
 
-let gun = new Gun({ peers: ['https://lonewolf-relay.herokuapp.com/gun'], axe: false, localStorage: true });
+let DEV_MODE = import.meta.env.DEV;
+
+let gun = new Gun({
+  peers: [
+    DEV_MODE
+      ? "http://localhost:8765/gun"
+      : "https://lonewolf-relay.herokuapp.com/gun",
+  ],
+  axe: false,
+  localStorage: true,
+});
 
 let user = gun.user().recall({ sessionStorage: true });
 
 export { gun, user };
+
