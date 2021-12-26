a182d2bc7ee4f29d99a617ab6d37e2d499faa0ec
Connor Davis <19422@kloofhigh.co.za>
2021-12-27 00:44:53 +0200

    Moved audio recordings to ipfs

--- state/gun.js
+++ state/gun.js
@@ -8,15 +8,11 @@ import "gun/lib/rindexed";
 let DEV_MODE = import.meta.env.DEV;
 
 let gun = new Gun({
-  peers: [
-    DEV_MODE ? ["http://localhost:8765/gun"] : "https://lone-wolf.software/gun",
-  ],
+  peers: ["https://relay.polaris.industries/gun", "https://lone-wolf.software/gun"],
   axe: false,
   localStorage: false,
   radisk: true,
 });
-
-gun.opt({ peers: ["https://relay.polaris.industries/gun"] });
 
 let user = gun.user().recall({ sessionStorage: true });
 
