e24382489e94725557697ce4677fd5ca55554443
Connor Davis <19422@kloofhigh.co.za>
2021-12-26 14:44:54 +0200

    0.1.0 - add new relay

--- state/gun.js
+++ state/gun.js
@@ -1,18 +1,16 @@
 import Gun from "gun/gun";
-import "gun/sea";
-import "gun/lib/radix";
 import "gun/lib/radisk";
-import "gun/lib/store";
+import "gun/lib/radix";
 import "gun/lib/rindexed";
+import "gun/lib/store";
+import "gun/sea";
 
 let DEV_MODE = import.meta.env.DEV;
 
 let gun = new Gun({
-  peers: [
-    DEV_MODE
-      ? "http://localhost:8765/gun"
-      : "https://lone-wolf.software/gun",
-  ],
+  peers: DEV_MODE
+    ? ["http://localhost:8765/gun"]
+    : ["https://lone-wolf.software/gun", "https://relay.polaris.industries/gun"],
   axe: false,
   localStorage: false,
   radisk: true,
