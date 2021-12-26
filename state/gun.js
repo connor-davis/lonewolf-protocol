027fe9a9186189fc42d14d19179df25666c39f63
Connor Davis <19422@kloofhigh.co.za>
2021-12-26 14:49:09 +0200

    0.1.1 fix peers

--- state/gun.js
+++ state/gun.js
@@ -1,20 +1,22 @@
 import Gun from "gun/gun";
-import "gun/lib/radisk";
-import "gun/lib/radix";
-import "gun/lib/rindexed";
-import "gun/lib/store";
 import "gun/sea";
+import "gun/lib/radix";
+import "gun/lib/radisk";
+import "gun/lib/store";
+import "gun/lib/rindexed";
 
 let DEV_MODE = import.meta.env.DEV;
 
 let gun = new Gun({
-  peers: DEV_MODE
-    ? ["http://localhost:8765/gun"]
-    : ["https://lone-wolf.software/gun", "https://relay.polaris.industries/gun"],
+  peers: [
+    DEV_MODE ? ["http://localhost:8765/gun"] : "https://lone-wolf.software/gun",
+  ],
   axe: false,
   localStorage: false,
   radisk: true,
 });
+
+gun.opt({ peers: ["https://relay.polaris.industries/gun"] });
 
 let user = gun.user().recall({ sessionStorage: true });
 
